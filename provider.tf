57748573d0ae16bfa6c233e0e296886b53d6754f
Ubuntu <ubuntu@ip-172-31-82-253.ec2.internal>
2023-10-05 11:19:22 +0000

    commiting

---
+++ provider.tf
@@ -0,0 +1,238 @@
+#AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.10.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+#vpc
+resource "aws_vpc" "this" {
+  cidr_block = "10.100.0.0/16"
+  tags = {
+    Name = "upgrad-vpc"
+  }
+}
+
+#public subnets
+resource "aws_subnet" "public1" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.100.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "upgrad-public-1"
+  }
+}
+resource "aws_subnet" "public2" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.100.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "upgrad-public-2"
+  }
+}
+
+#private subnets
+resource "aws_subnet" "private1" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.100.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "upgrad-private-1"
+  }
+}
+resource "aws_subnet" "private2" {
+  vpc_id     = aws_vpc.this.id
+  cidr_block = "10.100.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "upgrad-private-2"
+  }
+}
+
+#internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "upgrad-igw"
+  }
+}
+
+#elastic ip
+resource "aws_eip" "eip" {
+  domain = "vpc"
+}
+
+#nat gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
+  subnet_id     = aws_subnet.public1.id
+
+  tags = {
+    Name = "upgrad-nat"
+  }
+}
+
+#public route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+#private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+#route table association
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private.id
+}
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private.id
+}
+
+#security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+#EC2
+resource "aws_instance" "wordpress" {
+  ami                         = "ami-053b0d53c279acc90"
+  instance_type               = "t2.micro"
+  key_name                    = "mani"
+  subnet_id                   = aws_subnet.public1.id
+  security_groups             = [aws_security_group.allow_ssh.id]
+  associate_public_ip_address = true
+
+}
+
+#rds subnet
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+}
+#RDS INSTANCE
+resource "aws_db_instance" "rds_instance" {
+  engine                    = "mysql"
+  engine_version            = "5.7"
+  skip_final_snapshot       = true
+  final_snapshot_identifier = "my-final-snapshot"
+  instance_class            = "db.t2.micro"
+  allocated_storage         = 20
+  identifier                = "my-rds-instance"
+  db_name                   = "wordpress_db"
+  username                  = "manikanta"
+  password                  = "maniraju$"
+  db_subnet_group_name      = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids    = [aws_security_group.rds_security_group.id]
+
+  tags = {
+    Name = "RDS Instance"
+  }
+}
+# RDS security group
+resource "aws_security_group" "rds_security_group" {
+  name        = "rds-security-group"
+  description = "Security group for RDS instance"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.100.0.0/16"]
+  }
+
+  tags = {
+    Name = "RDS Security Group"
+  }
+}
+
+# Generate new private key
+resource "tls_private_key" "mani" {
+  algorithm = "RSA"
+}
+
+# Generate a key-pair with above key
+resource "aws_key_pair" "deployer" {
+  key_name   = "task3-key"
+  public_key = tls_private_key.mani.public_key_openssh
+}
+
+# Saving Key Pair
+resource "null_resource" "save_key_pair"  {
+	provisioner "local-exec" {
+	    command = "echo  ${tls_private_key.mani.private_key_pem} > mani.pem"
+  	}
+}
